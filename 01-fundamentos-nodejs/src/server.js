359f1e270ffd28f355c667c3a9090f7e8917c40e
Lucio <luciononato@hotmail.com>
2023-03-22 08:06:43 -0300

    refactor

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,6 +1,6 @@
 import { randomUUID } from "node:crypto";
 import http from "node:http";
-import { Database } from "./middlewares/database.js";
+import { Database } from "./database.js";
 import { json } from "./middlewares/json.js";
 
 const database = new Database();
