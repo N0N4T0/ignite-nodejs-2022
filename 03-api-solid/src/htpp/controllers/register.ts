16403522554505bc1a310bcb0cd5a6561055f4e2
Lucio <luciononato@hotmail.com>
2023-07-01 11:07:03 -0300

    feat: add register usecase

--- 03-api-solid/src/htpp/controllers/register.ts
+++ 03-api-solid/src/htpp/controllers/register.ts
@@ -1,8 +1,8 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 import { z } from 'zod'
-import { prisma } from '@/lib/prisma'
-import { hash } from 'bcryptjs'
+
+import { registerUsecase } from '@/use-cases/register'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -13,25 +13,15 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { email, name, password } = registerBodySchema.parse(request.body)
 
-  const password_hash = await hash(password, 6)
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-
-  if (userWithSameEmail) {
-    return reply.status(409).send()
-  }
-
-  await prisma.user.create({
-    data: {
+  try {
+    await registerUsecase({
       name,
       email,
-      password_hash,
-    },
-  })
+      password,
+    })
+  } catch (error) {
+    return reply.status(409).send()
+  }
 
   return reply.status(201).send()
 }
