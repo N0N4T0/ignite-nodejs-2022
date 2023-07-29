85cfe9769d41a5f853c6b048e93bdda78f66ffb7
Lucio <luciononato@hotmail.com>
2023-07-29 15:29:13 -0300

    fix: add tests and authenticate controller

---
+++ 03-api-solid/src/htpp/controllers/authenticate.ts
@@ -0,0 +1,37 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+import { z } from 'zod'
+
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { AuthenticateUseCase } from '@/use-cases/authenticate'
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(request.body)
+
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const authenticateUseCase = new AuthenticateUseCase(usersRepository)
+
+    await authenticateUseCase.execute({
+      email,
+      password,
+    })
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+
+    throw error
+  }
+
+  return reply.status(200).send()
+}
