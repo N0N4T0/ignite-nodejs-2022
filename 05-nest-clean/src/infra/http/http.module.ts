ff244f8cd3d2e59b4af1ea1ad11dffcfd1c6be20
Lucio <luciononato@hotmail.com>
2024-07-17 14:41:41 -0300

    fix: add fetch recent questions

--- 05-nest-clean/src/infra/http/http.module.ts
+++ 05-nest-clean/src/infra/http/http.module.ts
@@ -5,6 +5,7 @@ import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { DatabaseModule } from '../database/database.module'
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 
 @Module({
   imports: [DatabaseModule],
@@ -14,6 +15,6 @@ import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/crea
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
-  providers: [CreateQuestionUseCase],
+  providers: [CreateQuestionUseCase, FetchRecentQuestionsUseCase],
 })
 export class HttpModule {}
